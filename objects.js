7094f9bf73c576482768c7a3dd2df1c8c86d359d
LaTonya Johnson <latonya.johnson0921@gmail.com>
2019-08-07 18:24:25 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,1 +1,10 @@
-function destructiv
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
+  obj[key] = value
+  
+  return obj
+}
+
+const recipe = {eggs: 3}
+
+destructivelyUpdateObjectWithKeyAndValue(recipe, 'flour', '3 cups')
+
